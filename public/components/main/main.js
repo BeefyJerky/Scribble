8b2f19005496a1490f1979a8ff3041c4ac207e5a
DonnyLi <dongxiaoli1996@gmail.com>
2016-09-13 20:59:41 -0400

    Added ng-enter directive mainly used in the login form.

--- public/components/main/main.js
+++ public/components/main/main.js
@@ -61,3 +61,17 @@ app.run(function($rootScope, $location, $state, AuthService, RoomService, socket
     });
 
 });
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
