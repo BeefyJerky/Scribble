a3512f66c7614dcc73fac610740f21fe2672b7ea
DonnyLi <dongxiaoli1996@gmail.com>
2016-09-13 21:13:31 -0400

    Moved directives to a new folder.

---
+++ public/components/main/directives/mainDirectives.js
@@ -0,0 +1,15 @@
+var app = angular.module('main');
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
